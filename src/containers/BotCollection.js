3adaf20c6a282c6882f0bd8ea703f11db27be7dc
aseleidy <ash.etemadi@gmail.com>
2020-06-10 13:04:42 -0400

    core deliverables (not including bonus)

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import BotCard from '../components/BotCard'
+
 
 class BotCollection extends Component {
   //your code here
@@ -7,8 +9,9 @@ class BotCollection extends Component {
     return (
       <div className="ui four column grid">
         <div className="row">
-          {/*...and here..*/}
-          Collection of all bots
+          {this.props.bots.map(bot => {
+            return <BotCard bot={bot} handleClick={this.props.handleClick} deleteBot={this.props.deleteBot} /> 
+          })}
         </div>
       </div>
     );
